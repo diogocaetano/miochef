ba2c066b924c3818b3cb48adbd2a1b39d7d53860
Pedro Gabriel <eeevee@gmail.com>
2016-03-16 17:51:15 +0000

    ajuste no relacionamento de badges e chefs

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,5 +3,5 @@ class Badge < ActiveRecord::Base
 	validates :description, presence: true, uniqueness: true
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "64x64>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	has_many :chefs
+	has_and_belongs_to_many :chefs
 end
