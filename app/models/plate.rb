5a3496e92be415de13b7eaa562f1a81275ceb9bc
Pedro Gabriel <eeevee@gmail.com>
2016-03-18 13:58:34 +0000

    ajustes nos labels das views de pratos

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,7 +1,7 @@
 class Plate < ActiveRecord::Base
-  belongs_to :chef
+  # belongs_to :chef
   belongs_to :plate_type
-  belongs_to :plate_badge
-  belongs_to :ingredients
-  belongs_to :plate_accompaniment
+  has_many :plate_badge
+  has_and_belongs_to_many  :ingredients
+  has_and_belongs_to_many  :plate_accompaniment
 end
