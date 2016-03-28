41b9722541b6c4c0810d431d4d356a9a658b700e
Pedro Gabriel <eeevee@gmail.com>
2016-03-28 14:38:00 +0000

    correção no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -117,7 +117,7 @@ Permission.find_or_create_by(entity_id: 10, action_name: 'Remover', action: 'des
 
 # Pratos - 16/03/2016
 ModulesCategory.find_or_create_by(name: 'Pratos', icon: 'fa fa-spoon')
-	 = ModulesCategory.find_by(name: 'Pratos')
+pratosCategory = ModulesCategory.find_by(name: 'Pratos')
 
 Entity.find_or_create_by(controller: 'ingredients') do |entity|
 	entity.modules_category_id = pratosCategory.id
