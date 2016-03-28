4b960157e36f14d1c71be1d9570254fe71dd45d6
Diogo Caetano <diogo.caetano.alves@gmail.com>
2016-03-28 16:00:32 +0000

    automaticamente inicia o serve com 0.0.0.0, colocado variavel env de exemplo, mudado o database para receber da variavel

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,16 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+#make default  - server is listening on all interfaces (0.0.0.0)
+require 'rubygems'  
+require 'rails/commands/server'
+
+module Rails  
+  class Server
+    alias :default_options_bk :default_options
+    def default_options
+      default_options_bk.merge!(Host: '0.0.0.0')
+    end
+  end
+end  
