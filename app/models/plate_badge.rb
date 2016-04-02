0b0414c3a338eccf4110ff35a6bd2643cf0e076f
vagrant <vagrant@homestead>
2016-04-02 01:39:24 +0000

    ajustes de campos requeridos

--- app/models/plate_badge.rb
+++ app/models/plate_badge.rb
@@ -1,6 +1,7 @@
 class PlateBadge < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true
 	validates :description, presence: true, uniqueness: true
+	validates :image, presence: true
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "64x64>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	validates_attachment_size :image, less_than: 2.megabytes
