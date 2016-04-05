16c731409aa046b197a5370c67adb5fba3b15b0e
Pedro Gabriel <eeevee@gmail.com>
2016-04-05 20:05:31 +0000

    busca no módulo de pratos

--- app/controllers/plates_controller.rb
+++ app/controllers/plates_controller.rb
@@ -4,7 +4,16 @@ class PlatesController < ApplicationController
   # GET /plates
   # GET /plates.json
   def index
-    @plates = Plate.all
+    @term = params[:term]
+    @where = []    
+    @where << "plates.title LIKE :term"    
+    @where << "plates.description LIKE :term"    
+    @where << "plates.price LIKE :term"    
+    @where << "plates.available_quantity LIKE :term"    
+    @where << "plate_types.name LIKE :term"    
+    @where = @where.join(" OR ")
+
+    @plates = Plate.joins(:plate_type).where(@where, term: "%#{params[:term]}%").paginate(:page => params[:page], :per_page => 10)
   end
 
   # GET /plates/1
