1e6bf444b12f99cfed77c8f65d74d1207a6760ae
Pedro Gabriel <eeevee@gmail.com>
2016-04-08 13:58:56 +0000

    ajustes de layout de pratos e redirect back de ativar/desativar

--- app/controllers/plates_controller.rb
+++ app/controllers/plates_controller.rb
@@ -86,7 +86,7 @@ class PlatesController < ApplicationController
     @plate.active = 1
     @plate.save
     respond_to do |format|
-      format.html { redirect_to @plate, notice: 'Prato ativado com sucesso.' }
+      format.html { redirect_to :back, notice: 'Prato ativado com sucesso.' }
       format.json { head :no_content }
     end
   end
@@ -96,7 +96,7 @@ class PlatesController < ApplicationController
     @plate.active = 0
     @plate.save
     respond_to do |format|
-      format.html { redirect_to @plate, notice: 'Prato desativado com sucesso.' }
+      format.html { redirect_to :back, notice: 'Prato desativado com sucesso.' }
       format.json { head :no_content }
     end
   end
