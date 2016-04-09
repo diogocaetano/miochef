68e34f6abf4e54d178bb96d5edf2ed5a96a8d39b
eeevee <eeevee@gmail.com>
2016-04-09 16:57:00 -0300

    - validação de hora para janela de entrega

--- app/models/window_request.rb
+++ app/models/window_request.rb
@@ -1,4 +1,10 @@
 class WindowRequest < ActiveRecord::Base
 	validates :initial_time, presence: true
 	validates :final_time, presence: true
+
+	validate :is_initial_time_before_final_time?
+
+	def is_initial_time_before_final_time?
+		errors.add('Horário Inicial', 'deve ser menor que o Horário Final') unless self.initial_time < self.final_time
+	end
 end
