9e37e96f5c02a486e877c763efc36be9c648b49e
eeevee <eeevee@gmail.com>
2016-04-09 18:20:22 -0300

    - validações de rating

--- app/models/chef_rating.rb
+++ app/models/chef_rating.rb
@@ -3,4 +3,10 @@ class ChefRating < ActiveRecord::Base
   belongs_to :chef
 
   validates :client_id, :chef_id, :score, :presence => true
+
+  validate :client_already_rated_the_chef?
+
+  def client_already_rated_the_chef?
+  	errors.add('Cliente', 'já deu nota para este Chefe') unless ChefRating::where('client_id = ? AND chef_id = ?', self.client_id, self.chef_id).first.nil?
+  end
 end
--- app/models/plate_rating.rb
+++ app/models/plate_rating.rb
@@ -3,4 +3,10 @@ class PlateRating < ActiveRecord::Base
   belongs_to :plate
 
   validates :client_id, :plate_id, :score, :presence => true
+
+  validate :client_already_rated_the_plate?
+
+  def client_already_rated_the_plate?
+  	errors.add('Cliente', 'já deu nota para este Prato') unless PlateRating::where('client_id = ? AND plate_id = ?', self.client_id, self.plate_id).first.nil?
+  end
 end
