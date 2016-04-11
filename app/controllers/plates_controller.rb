519115df6d5756b1af3ae9857f6ae9094a17f92f
Diogo Caetano <diogo@umbuzeiro.com>
2016-04-10 21:38:55 -0300

    ajuste no redirect ao desativar prato

--- app/controllers/plates_controller.rb
+++ app/controllers/plates_controller.rb
@@ -98,7 +98,7 @@ class PlatesController < ApplicationController
     @plate.active = 0
     @plate.save
     respond_to do |format|
-      format.html { redirect_to back, :flash =>{:success => 'O prato foi desativado com sucesso.' } }
+      format.html { redirect_to :back, :flash =>{:success => 'O prato foi desativado com sucesso.' } }
       format.json { head :no_content }
     end
   end
