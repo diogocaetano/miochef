6d00bfc1d1a26799e966f898062ca63a8615e990
Luiz Paulo Valença <luizpaulo165@gmail.com>
2016-04-16 17:23:06 -0300

    Home - em andamento

---
+++ app/assets/javascripts/front/main.js
@@ -0,0 +1,16 @@
+Application = {
+	init: function () {
+    this.dateSelect();
+	},
+
+	// dateSelect
+	dateSelect: function () {
+		alert(0)
+	}
+
+};
+
+
+$(document).on("ready", function () {
+	Application.init();
+});
