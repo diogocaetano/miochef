b188fa91bb94a66d2186a725a1ac9119ac2b490c
Pedro Gabriel <eeevee@gmail.com>
2016-04-22 13:16:30 +0000

    adicionando migration para adiciona configuração de order_date_ranger

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415193422) do
+ActiveRecord::Schema.define(version: 20160422123758) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "chef_id",      limit: 4
