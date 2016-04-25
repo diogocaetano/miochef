3c8bd50256938da874665b4d84e84d790914d7c7
Pedro Gabriel <eeevee@gmail.com>
2016-04-25 13:31:16 +0000

    validação para não deixar salvar janelas com horários iguais

--- app/models/window_request.rb
+++ app/models/window_request.rb
@@ -5,7 +5,14 @@ class WindowRequest < ActiveRecord::Base
 
 	validate :is_initial_time_before_final_time?
 
+	validate :is_clone?, on: :create
+
 	def is_initial_time_before_final_time?
 		errors.add('Horário Inicial', 'deve ser menor que o Horário Final') unless self.initial_time < self.final_time
+	end
+
+	def is_clone?
+		window = WindowRequest::where(initial_time: self.initial_time).where(final_time: self.final_time).first
+		errors.add('Já existe', 'Janela de Entrega com esses horários') unless window.nil?
 	end
 end
