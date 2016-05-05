ee9482dd3739d91317d8aab026f4956c6b83c91c
Diogo Caetano <diogo@umbuzeiro.com>
2016-05-05 17:52:14 -0300

    adicionado permissoes de listar settings

--- db/migrate/20160412023727_add_settings_module_to_permissions.rb
+++ db/migrate/20160412023727_add_settings_module_to_permissions.rb
@@ -11,12 +11,14 @@ class AddSettingsModuleToPermissions < ActiveRecord::Migration
   		visible: true
   	)
 
-	Permission.create(entity_id: entity.id, action_name: 'Editar', action: 'edit')
-	Permission.create(entity_id: entity.id, action_name: 'Atualizar', action: 'update')	
-
-	Permission.where(entity_id: entity.id).all.each do |permission|
-		PermissionsRoles.create(role_id: 1, permission_id: permission.id)
-		PermissionsRoles.create(role_id: 2, permission_id: permission.id)
-	end
+  	Permission.create(entity_id: entity.id, action_name: 'Editar', action: 'edit')
+    Permission.create(entity_id: entity.id, action_name: 'Atualizar', action: 'update') 
+    Permission.create(entity_id: entity.id, action_name: 'Listar', action: 'index') 
+  	Permission.create(entity_id: entity.id, action_name: 'Visualizar', action: 'show')	
+    
+  	Permission.where(entity_id: entity.id).all.each do |permission|
+  		PermissionsRoles.create(role_id: 1, permission_id: permission.id)
+  		PermissionsRoles.create(role_id: 2, permission_id: permission.id)
+  	end
   end
 end
